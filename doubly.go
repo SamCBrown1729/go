d3baaed0219751153fc73db38707d0d47c7a2bef
Sam Brown <scbrown1729@gmail.com>
2025-10-17 13:26:41 +0100

    changed to use strings, might be broken

--- doubly.go
+++ doubly.go
@@ -9,7 +9,7 @@ import (
 
 type node struct {
 	freq int
-	char byte
+	char string
 	left *node
 	right *node
 }
@@ -17,56 +17,80 @@ type node struct {
 
 func cutNode(cut *node) {
 	
-	if newNode.right == nil {
-		newNode.left.right = nil
-	} else if newNode.left == nil {
-		newNode.right.left = nil
+	if cut.right == nil {
+		cut.left.right = nil
+	} else if cut.left == nil {
+		cut.right.left = nil
 	} else {
-		newNode.right.left = newNode.left
-		newNode.left.right = newNode.right
+		cut.right.left = cut.left
+		cut.left.right = cut.right
 	}
 
 }
 
+
 func insert(newNode, relativeNode *node, where string) {
-	if string == "right" {
-		
+	if where == "right" {
+		if relativeNode.right != nil{
+			newNode.right = relativeNode.right
+			newNode.right.left = newNode
+		} else {
+			newNode.right = nil
+		}
+
+		newNode.left = relativeNode
+		relativeNode.right = newNode
+	} else if where == "left" {
+		if relativeNode.left != nil{
+			newNode.left = relativeNode.left
+		 	newNode.left.right = newNode
+		} else {
+			newNode.left = nil
+		}
+
+		newNode.right = relativeNode
+		relativeNode.left = newNode
+	} else {
+		fmt.Println("That's not a direction")
+	}
+	
 }
 	
 
-
 func moveToHead(newNode *node) {
 	cutNode(newNode)
-	
+	largest := true
 	nextNode := newNode.left
 	for nextNode.left != nil {
 		if newNode.freq < nextNode.freq {
-			insert(newNode, relativeNode, right)
+			insert(newNode, nextNode, "right")
+			largest = false
 			break;
 		}
 		nextNode = nextNode.left
 	}
-	insert(newNode, nextNode, left)
+	if largest {
+		insert(newNode, nextNode, "left")
+	}
+
 }
 
-
-		
 
 func initialNodes(filename string) *node {	
 	dat, err := os.ReadFile(filename)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(dat)
 	
-	head := &node{freq: 1, char: dat[0]}
+	head := &node{freq: 1, char : string(dat[0])}
 	
 	tail := head
-	for _, letter := range dat[1:len(dat)-1]{
+	for _, letter := range dat[1:]{
+		fmt.Println(string(letter), letter)
 		foundLetter := false
 		newNode := tail 
 		for newNode.left != nil {
-			if newNode.char == letter {
+			if newNode.char == string(letter) {
 				newNode.freq += 1
 				foundLetter = true
 				moveToHead(newNode)
@@ -77,7 +101,7 @@ func initialNodes(filename string) *node {
 		
 		if foundLetter{continue}
 			
-		newNode = &node{freq : 1, char : letter}
+		newNode = &node{freq : 1, char : string(letter)}
 		tail.right = newNode
 		newNode.left = tail
 		tail = newNode
@@ -86,20 +110,45 @@ func initialNodes(filename string) *node {
 	return tail 
 
 }
-	
 
+
+func createTree(tail *node) (*node, *node){
+
+	rightNode := tail
+	leftNode := tail.left
+	tail = leftNode.left
+	newTreeNode := &node{rightNode.freq + leftNode.freq, leftNode.char + rightNode.char, leftNode, rightNode}
+	newNode := &node{freq : rightNode.freq + leftNode.freq, char : leftNode.char + rightNode.char, left : leftNode.left}
+	moveToHead(newNode)
+	if len(rightNode.char) == 1 {
+		rightNode.left = nil
+		rightNode.right = nil
+	}
+	if len(leftNode.char) == 1 {
+		leftNode.left = nil
+		leftNode.right = nil
+	}
+ 	return newTreeNode, tail
+}
+		
 
 func main() {
 	startNode := initialNodes("test.txt")
 	
-	nextNode := startNode
+dat, _ := os.ReadFile("test.txt")
+	fmt.Println(dat)	
+	treeNode, tail := createTree(startNode)
+	fmt.Println(*treeNode)
+	fmt.Println(*treeNode.right)
+	fmt.Println(*treeNode.left)
+	
+	nextNode := tail
+	fmt.Println(*nextNode)
 	for nextNode.left != nil {
-		fmt.Println(*nextNode)
 		nextNode = nextNode.left
+		fmt.Println(*nextNode)
 	}
-
-
+	
 }
-
 
 
